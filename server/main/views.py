9909d25cbf8fe412d8836a3257abcae88a793e64
Spoukster <spoukster@gmail.com>
2019-02-03 22:24:14 +0300

    Сделан импорт локальных urlpatterns в глобальные

--- server/main/views.py
+++ server/main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'main/index.html')
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'main/about.html')
+
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'main/contacts.html')
