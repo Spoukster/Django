8a8613371d7dfceb06ff3458039ca56a2bfb9e48
Spoukster <spoukster@gmail.com>
2019-02-07 17:56:31 +0300

    Создан список продуктов и выведен на страницу

--- server/products/views.py
+++ server/products/views.py
@@ -1,4 +1,11 @@
+import json
 from django.shortcuts import render
 
 def product_list_view(request):
-    return render(request, 'products/index.html')
+    with open('products/fixtures/data/data.json') as file:
+        data = json.load(file)
+        return render(
+            request,
+            'products/index.html',
+            {'object_list': data}
+        )
