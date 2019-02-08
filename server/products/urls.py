56669500411a34b3e7bdf8ffe684f9636777d2d0
Spoukster <spoukster@gmail.com>
2019-02-08 15:44:08 +0300

    Применены некоторые фильтры и тэги. Контент перенесен в базу даннных

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import product_list_view
+from .views import product_list_view, product_detail_view
 
 urlpatterns = [
-    path('', product_list_view),
+    path('', product_list_view, name='list'),
+    path('<int:pk>/', product_detail_view, name='detail')
 ]
--- server/products/views.py
+++ server/products/views.py
@@ -1,11 +1,28 @@
 import json
 from django.shortcuts import render
 
+from .models import Product
+
+
 def product_list_view(request):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
-        return render(
-            request,
-            'products/index.html',
-            {'object_list': data}
-        )
+    # with open('products/fixtures/data/data.json') as file:
+    #     data = json.load(file)
+    data = Product.objects.all()
+    return render(
+        request,
+        'products/index.html',
+        {'object_list': data}
+    )
+
+
+def product_detail_view(request, pk):
+    # with open('products/fixtures/data/data.json') as file:
+    #     data = json.load(file)
+    data = Product.objects.get(pk=pk)
+
+    return render(
+        request,
+        'products/detail.html',
+        {'object': data}
+        # {'object': data[idx]}
+    )
