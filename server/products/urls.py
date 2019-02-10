22d1b51c497794ab6e7895ddc6e2c15cec1bf9c5
Spoukster <spoukster@gmail.com>
2019-02-11 00:06:22 +0300

    Созданы API Create и Update для Category

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import product_list_view, product_detail_view
+from .views import product_list_view, product_detail_view, category_create_view, category_update_view
 
 urlpatterns = [
     path('', product_list_view, name='list'),
+    path('categories/create/', category_create_view, name='create'),
+    path('categories/<int:pk>/update', category_update_view, name='update'),
     path('<int:pk>/', product_detail_view, name='detail')
 ]
