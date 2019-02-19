cf30d84531e82d39e5542b345445e8cfde832081
Spoukster <spoukster@gmail.com>
2019-02-19 17:38:23 +0300

    Изменение авторизации пользователя на встроенный  LoginView

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from django.contrib.auth.views import LoginView
+
 from .views import (
     login_view , registration_view,
 )
@@ -8,6 +10,7 @@ from .views import (
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', login_view, name='login'),
+    # path('', login_view, name='login'),
+    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('registration', registration_view, name='registration')
 ]
